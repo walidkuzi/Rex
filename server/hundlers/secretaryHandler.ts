1748a4cd20589d54c6c19cfe76a34acf6a9a4c85
youssef-atallah <youssefajoo3@gmail.com>
2025-05-01 20:59:28 +0300

    Update handlers and routes

--- server/hundlers/secretaryHandler.ts
+++ server/hundlers/secretaryHandler.ts
@@ -7,8 +7,19 @@ import { Secretary } from '../types';
 
 
 // get all courses
-export const getCourses: RequestHandler = (req, res) => {
-  const secretaryId = req.body.secretaryId;
+export const getCourses: RequestHandler = (req, res) : any => {
+  const secretary = req.body.secretaryId;
+
+  // check if the secretary is valid
+  if (!secretary) {
+    return res.status(400).json({ error: 'Secretary is required' });
+  }
+  // check if the secretary is authorized
+  const authorized = db.getSecretaryById(secretary);
+  if (!authorized) {
+    return res.status(401).json({ error: 'Unauthorized secretary id' });
+  }
+  // get the courses
   const courses = db.getCourses();
   res.json(courses);
 };
